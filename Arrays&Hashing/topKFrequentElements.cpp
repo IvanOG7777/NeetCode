81ba192f97dd1266226779cf9a89fe5cf4140fa5
Ivan <elderivanargueta04@gmail.com>
2026-02-13 11:26:58 -0800

    topK

--- Arrays&Hashing/topKFrequentElements.cpp
+++ Arrays&Hashing/topKFrequentElements.cpp
@@ -6,18 +6,27 @@
 #include <vector>
 #include <map>
 
-
 class Solution {
-private:
-    std:: map<int,int> numsMap;
-    std:: vector<int> kNums;
 public:
     std:: vector<int> topKFrequent(std:: vector<int>& nums, int k) {
+
     }
 };
 
 
 int main() {
-    
 
+    std:: vector<int> nums = {1,2,2,3,3,3};
+    int k = 2; // return the 2 most frequent numbers, in this case it would be 2 and 3
+    std:: map<int,int> map;
+
+    for (auto &num : nums) {
+        map[num]++;
+    }
+
+    // map
+    //
+    // val: 1, count 1
+    // val: 2, count 2
+    // val: 3, count 3
 }
