e16e841235161331b10420f127725608323abc69
IvanOG7777 <elderivanargueta04@gmail.com>
2026-02-15 14:45:44 -0800

    product expect self almost solved
    Currently passing 14/19 cases. I need to check logic

--- Arrays&Hashing/productOfArrayExceptSelf.cpp
+++ Arrays&Hashing/productOfArrayExceptSelf.cpp
@@ -8,36 +8,50 @@
 class Solution {
 public:
     std:: vector<int> productExceptSelf(std:: vector<int>& nums) {
+        size_t n = nums.size();
+        std:: vector<int> prefixNums(n);
+        std:: vector<int> postfixNums(n);
+        std:: vector<int> output(n);
 
+        prefixNums[0] = nums[0];
+        postfixNums[n - 1] = nums[n - 1];
+
+        for (size_t i = 1; i < n; i++) {
+            prefixNums[i] = nums[i] * prefixNums[i - 1];
+        }
+        for (size_t i = n - 1; i > 0; i--) {
+            postfixNums[i - 1] = nums[i - 1] * postfixNums[i];
+        }
+
+        for (size_t i = 0; i < n; i++) {
+            if (i - 1 == -1) {
+                output[i] = 1 * postfixNums[i];
+                continue;
+            }
+
+            if (i >= n - 1) {
+                output[i] = prefixNums[i - 1];
+                continue;
+            }
+
+            output[i] = prefixNums[i-1] * postfixNums[i + 1];
+        }
+
+        return output;
     }
 };
 
 
 int main() {
-    std:: vector<int> nums = {1,2,3,4};
+    std:: vector<int> nums = {4,3,2,1,2};
+    Solution s;
     size_t n = nums.size();
     std:: vector<int> prefixNums(n);
     std:: vector<int> postfixNums(n);
+    std:: vector<int> output(n);
 
-    prefixNums[0] = nums[0];
-    postfixNums[n - 1] = nums[n - 1];
-
-    for (size_t i = 1; i < n; i++) {
-        prefixNums[i] = nums[i] * prefixNums[i - 1];
-    }
-
-    for (int i = n - 1; i > 0; i--) {
-        postfixNums[i] = postfixNums[i] * nums[i - 1];
-    }
-
-    for (auto &num : prefixNums) {
+    output = s.productExceptSelf(nums);
+    for (auto &num : output) {
         std:: cout << num << " ";
     }
-
-    std:: cout << std::endl;
-
-    for (auto &num : postfixNums) {
-        std:: cout << num << " ";
-    }
-
 }
