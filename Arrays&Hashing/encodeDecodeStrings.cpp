134993c45a590185dd86ab60c7396efc11c6e5e6
Ivan <elderivanargueta04@gmail.com>
2026-02-14 22:07:00 -0800

    init encode/decoode
    added some comments on how to solve

--- Arrays&Hashing/encodeDecodeStrings.cpp
+++ Arrays&Hashing/encodeDecodeStrings.cpp
@@ -17,7 +17,26 @@ public:
 };
 
 int main () {
-    std:: vector<std:: string> input = {"Hello", "world"};
-    
+    std:: vector<std:: string> strings = {"Hello", "world"};
+    std:: string outString;
+    size_t totalLength = 0;
+
+    for (auto &string : strings) {
+        size_t currentLength = string.length();
+        std:: cout << currentLength << std::endl;
+        totalLength += currentLength;
+        outString += string;
+    }
+
+    std:: cout << outString << std::endl;
+    std:: cout << totalLength << std::endl;
 
 }
+
+//get the size of the string
+// pass in the length + "string" to the out string
+
+// when decoding look for first number, remember it
+// while firstNumber seen
+// count the character after that until length
+// unsee the first one
