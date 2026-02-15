67fd87ff8b4945bc3f76ca8f384fb9bd445e9cbf
Ivan <elderivanargueta04@gmail.com>
2026-02-14 23:38:37 -0800

    Close to solving
    Need to add a case check for empty string

--- Arrays&Hashing/encodeDecodeStrings.cpp
+++ Arrays&Hashing/encodeDecodeStrings.cpp
@@ -4,39 +4,60 @@
 
 #include <iostream>
 #include <vector>
+
 class Solution {
 public:
 
     std:: string encode(std:: vector<std:: string>& strs) {
+        if (strs.empty()) return "";
+        std:: string outString = "";
+        for (auto &str : strs) {
+            if (str == "") {
+                size_t currentLength  = 1;
+                std:: string joinedString = (std::to_string(currentLength) + "#") + str;
+                outString += joinedString;
+                break;
+            }
+            size_t currentLength = str.length();
+            std:: string joinedString = (std::to_string(currentLength) + "#") + str;
+            outString += joinedString;
+        }
 
+        return outString;
     }
 
     std:: vector<std:: string> decode(std:: string s) {
+        std:: vector<std::string> decodedStrings;
+        bool seenFirstPound = false;
+        for (size_t i = 0; i < s.length(); i++) {
+            seenFirstPound = false;
 
+            if (s[i] == '#') seenFirstPound = true;
+
+            if (seenFirstPound) {
+                size_t substringLength = s[i - 1] - '0';
+                std:: string substring = s.substr(i + 1, substringLength);
+                decodedStrings.push_back(substring);
+                i += substringLength;
+            }
+        }
+
+        return decodedStrings;
     }
 };
 
 int main () {
-    std:: vector<std:: string> strings = {"Hello", "world"};
-    std:: string outString;
-    size_t totalLength = 0;
+    Solution solution;
+    std:: vector<std:: string> strs = {""};
+    std:: string encodedString = solution.encode(strs);
+    std:: vector<std::string> decodedStrings = solution.decode(encodedString);
 
-    for (auto &string : strings) {
-        size_t currentLength = string.length();
-        std:: cout << currentLength << std::endl;
-        totalLength += currentLength;
-        outString += string;
+    std:: cout << encodedString << std::endl;
+    std:: cout << std:: endl;
+
+    for (auto &str : decodedStrings) {
+        std:: cout << str << std::endl;
     }
 
-    std:: cout << outString << std::endl;
-    std:: cout << totalLength << std::endl;
 
 }
-
-//get the size of the string
-// pass in the length + "string" to the out string
-
-// when decoding look for first number, remember it
-// while firstNumber seen
-// count the character after that until length
-// unsee the first one
